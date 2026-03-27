77d97382534dee6ca8d897b7e5ca599f25466017
yz <youssef.zerroug1@gmail.com>
2026-03-27 12:08:40 -0700

    feat: add WeatherData and WeatherForecastDay models

---
+++ lib/models/weather_model.dart
@@ -0,0 +1,38 @@
+import 'diagnosis_model.dart';
+
+class WeatherForecastDay {
+  final String date;        // "YYYY-MM-DD"
+  final int rainChancePct;
+  final double minTempC;
+  final double maxTempC;
+
+  const WeatherForecastDay({
+    required this.date,
+    required this.rainChancePct,
+    required this.minTempC,
+    required this.maxTempC,
+  });
+
+  factory WeatherForecastDay.fromJson(Map<String, dynamic> j) =>
+      WeatherForecastDay(
+        date: j['date'] as String? ?? '',
+        rainChancePct: (j['rainChancePct'] as num? ?? 0).toInt(),
+        minTempC: (j['minTempC'] as num? ?? 0).toDouble(),
+        maxTempC: (j['maxTempC'] as num? ?? 0).toDouble(),
+      );
+}
+
+class WeatherData {
+  final WeatherSnapshot current;
+  final List<WeatherForecastDay> forecast;
+
+  const WeatherData({required this.current, required this.forecast});
+
+  factory WeatherData.fromJson(Map<String, dynamic> j) => WeatherData(
+    current: WeatherSnapshot.fromJson(
+        Map<String, dynamic>.from(j['current'] as Map)),
+    forecast: (j['forecast'] as List? ?? [])
+        .map((e) => WeatherForecastDay.fromJson(Map<String, dynamic>.from(e)))
+        .toList(),
+  );
+}
